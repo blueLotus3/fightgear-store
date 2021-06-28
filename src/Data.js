49a3b8a981327775eb256d7ff8e44a4c4e98c757
Troy Lemons Jr <troylemonsjr@gmail.com>
2021-06-28 13:16:50 -0700

    rendering data of items and created a shopping cart for products

---
+++ src/Data.js
@@ -0,0 +1,72 @@
+const Data = {
+    products: [
+        {
+            id:'1',
+            name:'Yokkao Boxing Gloves',
+            price: 69.99,
+            image: 'https://res.cloudinary.com/ds7w3ysag/image/upload/v1624909441/twins_boxing_gloves_o2tixr.jpg',
+        },
+        {
+            id:'2',
+            name:'Winnings Boxing Gloves',
+            price: 499.99,
+            image: 'https://res.cloudinary.com/ds7w3ysag/image/upload/c_scale,w_600/v1624909869/winning_boxing_gloves-753pvj_gfhc1k.jpg',
+        },
+        {
+            id:'3',
+            name:'Haybusa Boxing Gloves',
+            price: 99.99,
+            image: 'https://res.cloudinary.com/ds7w3ysag/image/upload/c_scale,w_600/v1624910019/hayabusa-marvel-captain-america-glove-main_marjvt.jpg',
+        },
+        {
+            id:'4',
+            name:'Cleto Reyes Boxing Gloves',
+            price: 179.99,
+            image: 'https://res.cloudinary.com/ds7w3ysag/image/upload/c_scale,w_600/v1624910353/cleto_reyes_kefseg.jpg',
+        },
+        {
+            id:'5',
+            name:'Grants Boxing Gloves',
+            price: 1400,
+            image: 'https://res.cloudinary.com/ds7w3ysag/image/upload/v1624909441/twins_boxing_gloves_o2tixr.jpg',
+        },
+        {
+            id:'6',
+            name:'Haybusa Gi',
+            price: 149.99,
+            image: 'https://res.cloudinary.com/ds7w3ysag/image/upload/c_scale,w_600/v1624910479/haybusa_gi_li4gbh.jpg',
+        },
+        {
+            id:'7',
+            name:'Venum Gi',
+            price: 149.99,
+            image: 'https://res.cloudinary.com/ds7w3ysag/image/upload/c_scale,w_600/v1624910605/venum_gi_lzn5ig.jpg',
+        },
+        {
+            id:'8',
+            name:'Haybusa Shin Guards',
+            price: 99.99,
+            image: 'https://res.cloudinary.com/ds7w3ysag/image/upload/c_scale,w_600/v1624910756/haybusa_shin_guards_gl32mi.jpg',
+        },
+        {
+            id:'9',
+            name:'Fairtex Shin guards',
+            price: 74.99,
+            image: 'https://res.cloudinary.com/ds7w3ysag/image/upload/c_scale,w_600/v1624910859/fairtex_shin_guards_ptmcbb.jpg',
+        },
+        {
+            id:'10',
+            name:'Twins Boxing Gloves',
+            price: 79.99,
+            image: 'https://res.cloudinary.com/ds7w3ysag/image/upload/c_scale,w_600/v1624910936/twins_dtblpg.jpg',
+        },
+        {
+            id:'11',
+            name:'Venum Mouthguard',
+            price: 29.99,
+            image: 'https://res.cloudinary.com/ds7w3ysag/image/upload/c_scale,w_600/v1624911007/mouthguard_cltndk.jpg',
+        },
+    ]
+}
+
+export default Data;
